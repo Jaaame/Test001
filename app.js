758053070d00ac908a3ff84da2414a95b2e150de
Jamme <6306021612015@fitm.kmutnb.ac.th>
2022-05-01 18:26:38 +0700

    up

--- app.js
+++ app.js
@@ -1,39 +1,84 @@
-const path = require("path");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
+const mongoose = require("mongoose");
 const express = require("express");
-const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+const methodOverride = require("method-override");
+const seedDb = require("./seed");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const localStrategy = require("passport-local");
 
-const errorController = require("./controllers/error");
+const productRoutes = require("./routes/product");
+const authRoutes = require("./routes/auth");
+const cartRoutes = require("./routes/cart");
+const paymentRoutes = require("./routes/payment");
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/admin");
 
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
 
-const app = express();
-
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log("OPSSSS Errorrrr");
+    console.log(err);
+  });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+// seedDb();
+
+const sessionConfig = {
+  secret: "ThisIsMySecrete",
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+//Initiolizing passport and session for storing user data
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Configuring passport for local strategy
+passport.use(new localStrategy(User.authenticate()));
+
+//Serializing and Deserializing User from session
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  User.findById("5dfa20ec1c9d440000bd5efd")
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      //req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
+  next();
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+app.use(productRoutes);
+app.use(authRoutes);
+app.use(cartRoutes);
+app.use(paymentRoutes);
+app.use(userRoutes);
+app.use(adminRoutes);
 
-app.use(errorController.get404);
-
-mongoConnect(() => {
-  app.listen(3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server started at port 3000");
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,44 @@
-const path = require("path");
-
 const express = require("express");
-
-const adminController = require("../controllers/admin");
-
 const router = express.Router();
+const { isLoggedIn } = require("../middlewere");
+const Order = require("../models/order");
 
-// /admin/add-product => GET
-router.get("/add-product", adminController.getAddProduct);
+router.get("/admin/:id/allorders", isLoggedIn, async (req, res) => {
+  try {
+    const allOrders = await Order.find({}).sort({ date: -1 });
+    res.render("admin/allOrders", { allOrders });
+  } catch (error) {
+    req.flash("error", "Cannot Find Orders");
+    res.render("error");
+  }
+});
 
-// /admin/products => GET
-router.get("/products", adminController.getProducts);
+router.get("/admin/:id/order/:orderId", isLoggedIn, async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.orderId);
+    res.render("admin/updateOrder", { order });
+  } catch (error) {
+    req.flash("error", "Unable to find the Order");
+    res.render("error");
+  }
+});
 
-// /admin/add-product => POST
-router.post("/add-product", adminController.postAddProduct);
-
-// /admin/edit-product => GET
-router.get("/edit-Product/:productId", adminController.getEditProduct);
-
-// /admin/edit-product => POST
-router.post("/edit-product", adminController.postEditProduct);
-
-router.post("/delete-product", adminController.postDeleteProduct);
+router.patch("/admin/:id/order/:orderId", isLoggedIn, async (req, res) => {
+  try {
+    await Order.updateOne(
+      { _id: req.params.orderId },
+      { $set: { status: req.body.status } },
+      {
+        new: true,
+        useFindAndModify: false,
+      }
+    );
+    req.flash("success", "Order Updated Successfully");
+    res.redirect(`/admin/${req.params.id}/allorders`);
+  } catch (error) {
+    req.flash("error", "Cannot Update Order");
+    res.render("error");
+  }
+});
 
 module.exports = router;
